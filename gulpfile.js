a2088832fd57839dec74d43c95d2eddb9bc84bd2
Marcus Warnsley <marcus.warnsley@gmail.com>
2016-02-23 15:04:39 -0500

    Setting up the gulp file js for defining task and making a default task for gulp as of right now

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+//Gulp.js configuration
+
+//include gulp and plugins
+
+var gulp = require('gulp');
+
+//file locations
+var source = 'source/',
+    dest = 'build/';
+
+//default task for gulp
+
+gulp.task('default', function(){
+
+});
